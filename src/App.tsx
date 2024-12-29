1d77ea5f767cb0de9b90572b61d0c591ab4faa75
Leejoongjae123 <ljj3347@naver.com>
2024-12-29 21:54:11 +0900

    '241229'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css'
-import { useTranslation } from 'react-i18next';
 import Navbar from './components/Navbar';
 import Contents from './components/Contents';
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    i18n.changeLanguage(e.target.value);
-  };
 
   return (
     <div className='w-full h-full bg-white'>
