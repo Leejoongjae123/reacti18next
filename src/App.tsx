90d795f23e44bfea8f23e6924559601c60b79eab
Leejoongjae123 <ljj3347@naver.com>
2024-12-31 16:21:39 +0900

    '123'

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import Contents from './components/Contents';
-import { useSearchStore } from './store/searchStore';
-import SearchList from './components/SearchList';
-import { useContentsStore } from './store/contentsStore';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Contents from "./components/Contents";
+import { useSearchStore } from "./store/searchStore";
+import SearchList from "./components/SearchList";
+import { useContentsStore } from "./store/contentsStore";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
 function App() {
   const { searchKeyword } = useSearchStore();
   console.log("searchKeyword:", searchKeyword);
   const { selectedContents } = useContentsStore();
   return (
-    <div className='w-full h-full bg-white'>
-      <Navbar>
-        {selectedContents === 'contents' ? <Contents /> : <SearchList />}
-      </Navbar>
-    </div>
-  )
+    <HelmetProvider>
+      <div className="w-full h-full bg-white">
+        <Helmet>
+          <title>User Guide</title>
+          <meta name="description" content="UCWORKS SCHOOL LINE User Guide" />
+        </Helmet>
+        <Navbar>
+          {selectedContents === "contents" ? <Contents /> : <SearchList />}
+        </Navbar>
+      </div>
+    </HelmetProvider>
+  );
 }
 
-export default App
+export default App;
